374dda2de3a40b6eededf835138cba95eacd1e1e
jpagett <pagett.jared@gmail.com>
2021-09-15 11:44:32 -0700

    updated findVarField to recognize top-level atomdata fields (e.g., summedODy)

--- custom_functions/data_fcns/findVarField.m
+++ custom_functions/data_fcns/findVarField.m
@@ -1,9 +1,10 @@
 function [whereThisVarLives, isRunDataVar, isNCVar, isAtomdataVar]  = findVarField(RunDatas, var_name)
 % FINDVARFIELD determines where a given variable name resides by checking the
 % fields of RunDatas.vars, RunDatas.Atomdata.vars, and RunDatas.ncVars.
-% First output is 'runDataVars', 'ncVars', or 'atomdataVars'.
+% First output is 'runDataVars', 'ncVars', 'atomdataVars', or 'atomdata'.
 %
-% If field does not exist in vars, ncVars, or atomdata.vars, throws error.
+% If field does not exist in vars, ncVars, atomdata.vars, or atomdata,
+% throws error.
 %
 % Field will be rejected as existing from a particular location if it is
 % missing from any of the input RunDatas.
@@ -11,6 +12,8 @@ function [whereThisVarLives, isRunDataVar, isNCVar, isAtomdataVar]  = findVarFie
 % If 'runDataVars', variable exists in all RunDatas.vars.
 % If 'ncVars', variable exists in all RunDatas.ncVars.
 % If 'atomdataVars', variable exists in all RunDatas.Atomdata.vars.
+% If 'atomdata', variable exists in all RunDatas.Atomdata as a top-level
+% field.
 %
 % If field exists in both RunDatas.vars and in Atomdata.vars, returns
 % 'runDataVars'.
@@ -18,6 +21,7 @@ function [whereThisVarLives, isRunDataVar, isNCVar, isAtomdataVar]  = findVarFie
     isRunDataVar = all(cellfun(@(x) isfield(x.vars,var_name), RunDatas));
     isNCVar = all(cellfun(@(x) isfield(x.ncVars,var_name), RunDatas));
     isAtomdataVar = all(cellfun( @(rdcells) all(arrayfun( @(ad) isfield(ad.vars,var_name), rdcells.Atomdata)), RunDatas));
+    isAtomdataField = all(cellfun( @(rdcells) all(arrayfun( @(ad) isfield(ad,var_name), rdcells.Atomdata)), RunDatas));
     
     if isRunDataVar
         whereThisVarLives = 'runDataVars';
@@ -25,9 +29,11 @@ function [whereThisVarLives, isRunDataVar, isNCVar, isAtomdataVar]  = findVarFie
         whereThisVarLives = 'ncVars';
     elseif isAtomdataVar
         whereThisVarLives = 'atomdataVars';
+    elseif isAtomdataField
+        whereThisVarLives = 'atomdata';
     end
                 
-    if ~isRunDataVar && ~isNCVar && ~isAtomdataVar
+    if ~isRunDataVar && ~isNCVar && ~isAtomdataVar && ~isAtomdataField
         errMsg = strcat(...
             "The variable ",var_name,...
             " is not a field in any of vars, ncVars, or atomdata.vars",...
